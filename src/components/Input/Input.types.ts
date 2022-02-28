d6a9b34cd1a462bb075e64f2155083b4f0d07b16
Alisson Macedo <alisson.macedo@alfreddelivery.com>
2022-02-28 00:22:52 -0300

    feat: add Theme provider light and dark colors

--- src/components/Input/Input.types.ts
+++ src/components/Input/Input.types.ts
@@ -3,5 +3,5 @@ import { IconBaseProps } from 'react-icons';
 
 export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
-  icon: React.ComponentType<IconBaseProps>;
+  icon?: React.ComponentType<IconBaseProps>;
 }
