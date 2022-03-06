040769ea6172b380785434d083159e117524a6f4
Alisson Macedo <alisson.macedo@alfreddelivery.com>
2022-03-06 15:49:00 -0300

    feat: add routing in map

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,32 +1,172 @@
-import { withGoogleMap, GoogleMap, withScriptjs } from 'react-google-maps';
+/* eslint-disable no-undef */
+import React, { useMemo, useState, useCallback } from 'react';
+import {
+  GoogleMap,
+  Marker,
+  LoadScript,
+  StandaloneSearchBox,
+  DirectionsService,
+  DirectionsRenderer,
+} from '@react-google-maps/api';
+import { Container, Context } from './styles';
 
-const Map = withScriptjs(
-  withGoogleMap(() => {
-    return (
-      <GoogleMap
-        defaultZoom={15}
-        defaultCenter={{
-          lat: -21.168374,
-          lng: -47.75153,
-        }}
-        defaultOptions={{
-          mapTypeControl: false,
-          streetViewControl: false,
-          fullscreenControl: false,
-        }}
-      />
-    );
-  }),
-);
+const Map: React.FC = () => {
+  // const { isLoaded } = useJsApiLoader({
+  //   id: 'google-map-script',
+  //   googleMapsApiKey: `${process.env.REACT_APP_GOOGLE_API_KEY}`,
+  // });
 
-const MapComponent = () => {
+  const [map, setMap] = useState<google.maps.Map>();
+  const [searchBoxA, setSearchBoxA] =
+    React.useState<google.maps.places.SearchBox>();
+  const [searchBoxB, setSearchBoxB] =
+    React.useState<google.maps.places.SearchBox>();
+
+  const [pointA, setPointA] = useState<google.maps.LatLngLiteral>();
+  const [pointB, setPointB] = useState<google.maps.LatLngLiteral>();
+
+  const [origin, setOrigin] = useState<google.maps.LatLngLiteral | null>();
+  const [destination, setDestination] =
+    useState<google.maps.LatLngLiteral | null>();
+
+  const [response, setResponse] =
+    useState<google.maps.DistanceMatrixResponse | null>();
+
+  const latLng = { lat: -21.168434, lng: -47.751594 };
+
+  const onLoadA = (ref: google.maps.places.SearchBox) => {
+    setSearchBoxA(ref);
+  };
+
+  const onLoadB = (ref: google.maps.places.SearchBox) => {
+    setSearchBoxB(ref);
+  };
+
+  const onMapLoad = (m: google.maps.Map) => {
+    setMap(m);
+  };
+
+  const onPlacesChangedA = () => {
+    const places = searchBoxA!.getPlaces();
+    console.log(places);
+    const place = places![0];
+    const location = {
+      lat: place?.geometry?.location?.lat() || 0,
+      lng: place?.geometry?.location?.lng() || 0,
+    };
+    setPointA(location);
+    setOrigin(null);
+    setDestination(null);
+    setResponse(null);
+    map?.panTo(location);
+  };
+
+  const onPlacesChangedB = () => {
+    const places = searchBoxB!.getPlaces();
+    console.log('places', places);
+    const place = places![0];
+    const location = {
+      lat: place?.geometry?.location?.lat() || 0,
+      lng: place?.geometry?.location?.lng() || 0,
+    };
+
+    setPointB(location);
+    setOrigin(null);
+    setDestination(null);
+    setResponse(null);
+
+    map?.panTo(location);
+  };
+
+  const traceRoute = () => {
+    if (pointA && pointB) {
+      setOrigin(pointA);
+      setDestination(pointB);
+    }
+  };
+
+  const clearRoute = () => {
+    setPointA(undefined);
+    setPointB(undefined);
+    setOrigin(null);
+    setDestination(null);
+    setResponse(null);
+  };
+
+  const directionsServiceOptions =
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    useMemo<google.maps.DirectionsRequest>(() => {
+      return {
+        origin,
+        destination,
+        travelMode: 'DRIVING',
+      };
+    }, [origin, destination]);
+
+  const directionsCallback = useCallback(res => {
+    if (res !== null && res.status === 'OK') {
+      console.log('res', res);
+      setResponse(res);
+    } else {
+      console.log('res', res);
+    }
+  }, []);
+
+  const directionsRenderOptions = useMemo<any>(() => {
+    return {
+      directions: response,
+    };
+  }, [response]);
+
   return (
-    <Map
-      googleMapURL="AIzaSyBffb-JobxOiAyBbg39zvx_duIo8NOAmxg"
-      loadingElement={<div style={{ height: `100%` }} />}
-      containerElement={<div style={{ height: '100%' }} />}
-      mapElement={<div style={{ height: `100%` }} />}
-    />
+    <LoadScript
+      googleMapsApiKey="AIzaSyBffb-JobxOiAyBbg39zvx_duIo8NOAmxg"
+      libraries={['places']}
+    >
+      <Context>
+        <StandaloneSearchBox
+          onLoad={onLoadA}
+          onPlacesChanged={onPlacesChangedA}
+        >
+          <input placeholder="Search" />
+        </StandaloneSearchBox>
+        <StandaloneSearchBox
+          onLoad={onLoadB}
+          onPlacesChanged={onPlacesChangedB}
+        >
+          <input placeholder="Search" />
+        </StandaloneSearchBox>
+        <button type="button" onClick={traceRoute}>
+          Traçar rota
+        </button>
+        <button type="button" onClick={clearRoute}>
+          Limpar
+        </button>
+      </Context>
+      <Container>
+        <GoogleMap
+          onLoad={onMapLoad}
+          mapContainerStyle={{ width: '100%', height: '100%' }}
+          center={latLng}
+          zoom={15}
+        >
+          {!response && pointA && <Marker position={pointA} />}
+          {!response && pointB && <Marker position={pointB} />}
+
+          {origin && destination && (
+            <DirectionsService
+              options={directionsServiceOptions}
+              callback={directionsCallback}
+            />
+          )}
+
+          {response && directionsRenderOptions && (
+            <DirectionsRenderer options={directionsRenderOptions} />
+          )}
+        </GoogleMap>
+      </Container>
+    </LoadScript>
   );
 };
-export default MapComponent;
+export default Map;
