69a5ede05a8b3403740bf1acfcc61bd97bc11e98
Alisson Macedo <alisson.macedo@alfreddelivery.com>
2022-03-13 14:22:54 -0300

    feat: add teste for update user

--- src/__tests__/hooks/auth.spec.tsx
+++ src/__tests__/hooks/auth.spec.tsx
@@ -1,4 +1,4 @@
-import { renderHook } from '@testing-library/react-hooks';
+import { renderHook, act } from '@testing-library/react-hooks';
 import MockAdapter from 'axios-mock-adapter';
 import { AuthProvider, useAuth } from '../../hooks/auth';
 
@@ -6,6 +6,10 @@ import api from '../../services/api';
 import loginDataObject from '../../helpers/mockData/testSignIn';
 
 const apiMock = new MockAdapter(api);
+interface AuthState {
+  token: string;
+  user: string;
+}
 
 describe('Auth hook', () => {
   it('should be able to sign in', async () => {
@@ -30,9 +34,77 @@ describe('Auth hook', () => {
     );
     expect(setItemSpy).toHaveBeenCalledWith(
       '@PainelAlfred:user',
-      JSON.stringify(loginDataObject.data.user),
+      loginDataObject.data.user.data.email,
     );
 
     expect(result.current.user).toEqual('test@example.com');
+  });
+
+  it('should restore saveed data from storage when auth inits', () => {
+    jest.spyOn(Storage.prototype, 'getItem').mockImplementation(key => {
+      switch (key) {
+        case '@PainelAlfred:token':
+          return 'token-123';
+        case '@PainelAlfred:user':
+          return 'test@example.com';
+        default:
+          return null;
+      }
+    });
+    const { result, waitForNextUpdate } = renderHook(() => useAuth(), {
+      wrapper: AuthProvider,
+    });
+
+    expect(result.current.user).toEqual('test@example.com');
+  });
+
+  it('should be able to sig  out', async () => {
+    jest.spyOn(Storage.prototype, 'getItem').mockImplementation(key => {
+      switch (key) {
+        case '@PainelAlfred:token':
+          return 'token-123';
+        case '@PainelAlfred:user':
+          return 'test@example.com';
+        default:
+          return null;
+      }
+    });
+
+    const removeItemSpy = jest.spyOn(Storage.prototype, 'removeItem');
+
+    const { result } = renderHook(() => useAuth(), {
+      wrapper: AuthProvider,
+    });
+
+    act(() => {
+      result.current.signOut();
+    });
+
+    expect(removeItemSpy).toBeCalledTimes(3);
+    expect(result.current.user).toBeUndefined();
+  });
+
+  it('should be able to update user data', () => {
+    const setItemSpy = jest.spyOn(Storage.prototype, 'setItem');
+
+    const { result } = renderHook(() => useAuth(), {
+      wrapper: AuthProvider,
+    });
+
+    const authData = {
+      user: 'test@example.com',
+      token: '123',
+    };
+
+    act(() => {
+      result.current.updateUser(authData);
+    });
+
+    expect(setItemSpy).toHaveBeenCalledWith(
+      '@PainelAlfred:user',
+      authData.user,
+    );
+
+    expect(result.current.user).toEqual(authData.user);
   });
 });
