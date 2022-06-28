fdf9eb1326c0be5890bfeb27c08030d199645c23
Alisson Macedo <alisson.macedo@alfreddelivery.com>
2022-06-28 17:56:40 -0300

    refactor: refact initial obj

---
+++ src/components/molecules/NewAddress/initial.ts
@@ -0,0 +1,16 @@
+export const initial = {
+  delivery: {
+    id: Math.random(),
+    street: '',
+    number: '',
+    complement: '',
+    neighborhood: '',
+    city: '',
+    state: '',
+    longitude: 0,
+    latitude: 0,
+    obs: '',
+    address: '',
+    payment: 0,
+  },
+};
